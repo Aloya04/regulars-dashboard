41411ff4cbdde2e33c46cf574e5c962feae24460
Aloya04 <alicjabal.edu@gmail.com>
2025-05-17 01:23:33 +0200

    predeploy

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// we're using firestore, which is firebase's database.
+import { getFirestore, enableIndexedDbPersistence} from "firebase/firestore"; 
 
-// Your web app's Firebase configuration
+// this is your unique configuration info from firebase.
+// it tells your app which firebase project to connect to.
 const firebaseConfig = {
-  apiKey: "AIzaSyAe6TwG97dPu-tQPgSH3VIUiCqspK91orU",
+  apiKey: "AIzaSyAe6TwG97dPu-tQPgSH3VIUiCqspK91orU", 
   authDomain: "regulars-3a4e8.firebaseapp.com",
   projectId: "regulars-3a4e8",
   storageBucket: "regulars-3a4e8.firebasestorage.app",
@@ -13,5 +13,23 @@ const firebaseConfig = {
   appId: "1:918091340439:web:b2760681e340106678a7d2"
 };
 
-// Initialize Firebase
+// initialize the firebase app with our config.
 const app = initializeApp(firebaseConfig);
+
+// get a reference to the firestore database service.
+const db = getFirestore(app);
+
+// 
+// this tries to enable offline persistence - means app can still work with some data even if the internet connection is lost.
+enableIndexedDbPersistence(db)
+  .catch((err) => {
+    // if it fails, we're not logging detailed errors to the console anymore for simplicity.
+    if (err.code === 'failed-precondition') {
+      // this can happen if multiple tabs are open.
+    } else if (err.code === 'unimplemented') {
+      // this means the browser doesn't support it.
+    }
+  });
+
+// we export 'db' so other parts of our app can use it to interact with the database.
+export { db, app };
